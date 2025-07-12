b8218094097bc4cc41110298ab31c462a3482e7a
Atharva <atharvaanbhule@gmail.com>
2025-07-12 16:58:00 +0530

    done all

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/authRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cors from "cors";
 import orderRoutes from "./routes/orderRoutes.js";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import productRoutes from "./routes/productRoutes.js";
 import Product from "./models/Product.js";
 dotenv.config();
@@ -21,19 +21,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //mongo
-mongoose.connect("mongodb://localhost:27017/ecom", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err))
+mongoose
+  .connect("mongodb://localhost:27017/ecom", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Routes
 app.use("/", authRoutes);
 
 // Add this with your other app.use() routes
 app.use("/api/orders", orderRoutes);
-app.use("/api",productRoutes)
+app.use("/api", productRoutes);
 
 // Error handling
 app.use(notFound);
